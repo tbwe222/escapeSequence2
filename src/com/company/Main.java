585bb404e749b1dc37dca6f932e629cb0e3280dd
GBTC441003UR <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-04-17 13:29:51 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Cost\t  Quantity\tTotal\n$1,000.00   4\t\t$4,000.00 \n$   50.00   8\t\t  $400.00\n______________________________\nTOTAL:\t\t12\t\t$4,400.00");
+    }
+}
