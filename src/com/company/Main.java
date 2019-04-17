ae57e3ead25a395ddc4d2500f4539a3dc860ad07
GBTC441003UR <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-04-17 14:37:30 -0400

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Cost\t  Quantity\tTotal\n$1,000.00   4\t\t$4,000.00 \n$   50.00   8\t\t  $400.00\n______________________________\nTOTAL:\t\t12\t\t$4,400.00");
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How is the weather?");
+        String answer = sc.nextLine();
+
+        if (answer.equals("rain"))
+            System.out.println("Take your umbrella!");
+        else if (answer.equals("stormy"))
+            System.out.println("you should stay inside!");
+        else if (answer.equals("windy"))
+            System.out.println("Wear your jacket!");
+        else if (answer.equals("snow") )
+            System.out.println("Wear a coat and take a shovel!");
+        else
+            System.out.println("Enjoy your day!");
     }
+
 }
